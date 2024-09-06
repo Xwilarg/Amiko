7841215607777e1d5653f6b8247558d5a870e718
Xwilarg <xwilarg@protonmail.com>
2024-09-06 18:14:44 +0200

    Add server

--- Amiko.Client/MainPage.xaml.cs
+++ Amiko.Client/MainPage.xaml.cs
@@ -1,24 +1,74 @@
-﻿namespace Amiko.Client
+﻿using System.Net.WebSockets;
+using System.Text;
+
+namespace Amiko.Client
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        ClientWebSocket sock;
 
         public MainPage()
         {
             InitializeComponent();
+
+            Appearing += async (s, e) =>
+            {
+                sock = new();
+                await ConnectAsync();
+                _ = Task.Run(ListenAsync);
+            };
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async Task ListenAsync()
         {
-            count++;
+            while (true)
+            {
+                var buffer = new byte[1024];
+                var response = await sock.ReceiveAsync(buffer, CancellationToken.None);
+                buffer = buffer.TakeWhile((v, index) => buffer.Skip(index).Any(w => w != 0x00)).ToArray(); // TODO: Need proper protocol
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Data.Text += $"> {Encoding.UTF8.GetString(buffer)}\n";
+                });
+            }
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private async Task ConnectAsync()
+        {
+            try
+            {
+                await sock.ConnectAsync(new("ws://localhost:5000/ws"), CancellationToken.None);
+                SendButton.IsEnabled = true;
+                Data.Text += "Connected\n";
+                //SemanticScreenReader.Announce("Connected");
+            }
+            catch (Exception ex)
+            {
+                Data.Text += $"Error while connecting: {ex.Message}\n";
+            }
+        }
+
+        private void OnSendMessage(object sender, EventArgs e)
+        {
+            var msg = Input.Text;
+            Input.Text = string.Empty;
+            Data.Text += $"< {msg}\n";
+
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    var encoded = Encoding.UTF8.GetBytes(msg);
+                    var buff = new ArraySegment<byte>(encoded, 0, encoded.Length);
+                    await sock.SendAsync(buff, WebSocketMessageType.Text, true, CancellationToken.None);
+                    //SemanticScreenReader.Announce("Message sent");
+                }
+                catch (Exception ex)
+                {
+                    Data.Text += $"Error while sending message: {ex.Message}\n";
+                }
+            });
         }
     }
 
---
+++ Amiko.Server/Controllers/WebsocketController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace Amiko.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WebsocketController : ControllerBase
+    {
+        private readonly ILogger<WebsocketController> _logger;
+
+        public WebsocketController(ILogger<WebsocketController> logger)
+        {
+            _logger = logger;
+        }
+
+        private static readonly List<WebSocket> _sockets = new();
+
+        [Route("/ws")]
+        public async Task Get()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                var client = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                lock (_sockets)
+                {
+                    _sockets.Add(client);
+                }
+
+                while (true)
+                {
+                    var buffer = new byte[1024];
+                    var response = await client.ReceiveAsync(buffer, CancellationToken.None);
+
+                    if (response.MessageType == WebSocketMessageType.Text)
+                    {
+                        _logger.Log(LogLevel.Information, Encoding.UTF8.GetString(buffer));
+
+                        List<Task> tasks = [];
+                        lock (_sockets)
+                        {
+                            foreach (var s in _sockets)
+                            {
+                                Task t = s.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+                                tasks.Add(t);
+                            }
+                        }
+                        foreach (var t in tasks)
+                        {
+                            try
+                            {
+                                await t;
+                            }
+                            catch (Exception e)
+                            { }
+                        }
+                    }
+                    else if (response.MessageType == WebSocketMessageType.Close)
+                    {
+                        lock (_sockets)
+                        {
+                            _sockets.Remove(client);
+                        }
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+    }
+}
