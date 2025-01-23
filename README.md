b5bd6e236f2306e21537cd4d6cc22c125d2bf949
Xwilarg <xwilarg@protonmail.com>
2025-01-23 18:37:44 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,29 @@
+Chat software
+
+## Backend
+
+### nginx
+
+```
+server {
+	root /home/path/to/amiko;
+
+	index index.html;
+
+	server_name amiko.host.com;
+
+	location / {
+		try_files $uri $uri/ =404;
+	}
+	location /ws {
+        proxy_pass http://localhost:5129;
+		proxy_http_version          1.1;
+		proxy_set_header Upgrade    $http_upgrade;
+		proxy_set_header Connection "upgrade";
+    }
+}
+```
+
+## Fontend
+
+`npm i` then `npm run start`
--- client/js/preload.js
+++ client/js/preload.js
@@ -46,7 +46,7 @@ window.addEventListener('DOMContentLoaded', () => {
     sendSystemMessage(`Chrome v${process.versions["chrome"]}, Node v${process.versions["node"]}, Electron v${process.versions["electron"]}`);
 
     const socket = new WebSocket("ws://localhost:5129/ws");
-    // const socket = new WebSocket("ws://amiko.zirk.eu/ws");
+    //const socket = new WebSocket("wss://amiko.zirk.eu/ws");
 
     // Connection opened
     socket.addEventListener("open", (_) => {
