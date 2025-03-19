8edf5717af1f87a28d68255dfae29292a2a4454b
Christian Chaux <xwilarg@protonmail.com>
2025-03-19 14:59:27 +0000

    Improve markdown

--- client/src/renderer.js
+++ client/src/renderer.js
@@ -73,6 +73,7 @@ function parseMessage(msg) {
     finalHtml = finalHtml.replaceAll(/(https?:\/\/([^ \n]+))/gm, '<span class="link">$1</span>');
 
     finalHtml = finalHtml.replaceAll(/```\n?(([^`]+`{0,2})*)```/gm, '<pre>$1</pre>');
+    finalHtml = finalHtml.replaceAll(/`([^*]+)`/gm, '<code>$1</code>');
     finalHtml = finalHtml.replaceAll(/^&gt; ([^\n]+)/gm, '<pre>$1</pre>');
     finalHtml = finalHtml.replaceAll(/\*\*(([^*]+\*{0,1})*)\*\*/gm, '<b>$1</b>');
     finalHtml = finalHtml.replaceAll(/\*([^*]+)\*/gm, '<i>$1</i>');
