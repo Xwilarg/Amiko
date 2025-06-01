1c1732cd3c7c1cea605093578550092cc76bd748
Xwilarg <xwilarg@protonmail.com>
2025-06-01 14:03:52 +0200

    Fix call to db init

--- server/Amiko.Server/Program.cs
+++ server/Amiko.Server/Program.cs
@@ -73,7 +73,8 @@ public class Program
 
         var app = builder.Build();
 
-        ContextInterpreter.Get(app.Services.GetRequiredService<SqliteContext>()).Init();
+        using var scope = app.Services.CreateScope();
+        ContextInterpreter.Get(scope.ServiceProvider.GetRequiredService<SqliteContext>()).Init();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
