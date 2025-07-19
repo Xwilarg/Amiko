6597eceb84430fcd284ded6db136d3c98d3405ec
Xwilarg <xwilarg@protonmail.com>
2025-07-19 13:37:21 +0200

    Clean browser code

--- client/src/platform/browser.js
+++ client/src/platform/browser.js
@@ -9,80 +9,73 @@ async function writePrefAsync(key, value) {
 }
 
 let canUseNotification = false;
-function initBrowser() {
-    try
-    {
-        window.Notification.requestPermission().then(function (permission) {
-            console.log(`Notification perm status: ${permission}`);
-        });
-        canUseNotification = true;
-    }
-    catch
-    {
-        console.warn("Notification API not available");
-    }
-
-    navigator.permissions.query({ name: "clipboard-read" }).then((result) => {
-        console.log(`Clipboard perm status: ${result.state}`);
-        if (result.state === "prompt") { // Show prompt right away so no need to ask in the future
-            navigator.clipboard.read();
-        }
+try
+{
+    window.Notification.requestPermission().then(function (permission) {
+        console.log(`Notification perm status: ${permission}`);
     });
+    canUseNotification = true;
+}
+catch
+{
+    console.warn("Notification API not available");
+}
+
+navigator.permissions.query({ name: "clipboard-read" }).then((result) => {
+    console.log(`Clipboard perm status: ${result.state}`);
+    if (result.state === "prompt") { // Show prompt right away so no need to ask in the future
+        navigator.clipboard.read();
+    }
+});
     
-    compatibility = {
-        notification: () => canUseNotification,
-        crossorigin: () => false
-    };
-    versions = {
-        node: () => null,
-        chrome: () => navigator.userAgent,
-        electron: () => null
-    };
-    interaction = {
-        open: (url) => window.open(url, '_blank').focus()
-    };
-    configuration = {
-        baseUrl: () => location.host
-    };
-    filesystem = {
-        readTokenAsync: async () => {
-            const pref = await readPrefAsync("websites", "");
-            if (pref === "") return {};
+compatibility = {
+    notification: () => canUseNotification,
+    crossorigin: () => false
+};
+versions = {
+    node: () => null,
+    chrome: () => navigator.userAgent,
+    electron: () => null
+};
+interaction = {
+    open: (url) => window.open(url, '_blank').focus()
+};
+configuration = {
+    baseUrl: () => location.host
+};
+filesystem = {
+    readTokenAsync: async () => {
+        const pref = await readPrefAsync("websites", "");
+        if (pref === "") return {};
 
-            let data = {};
-            for (let website of pref.split(','))
-            {
-                data[website] = await readPrefAsync(`website-${website}`, "");
-            }
-            return data;
-        },
-        writeTokenAsync: async (token, website) => {
-            const pref = await readPrefAsync("websites", "");
-            let websites;
-            if (pref === "") websites = [];
-            else websites = pref.split(',');
-            websites.push(website);
-
-            await writePrefAsync("websites", websites.join(','));
-            await writePrefAsync(`website-${website}`, token);
-        },
-        readPrefAsync: readPrefAsync,
-        writePrefAsync: writePrefAsync,
-        readPrefArrayAsync: async (key) => {
-            const pref = await readPrefAsync(key, "");
-            if (pref === "") return [];
-            return pref.split(",");
-        },
-        writePrefArrayAsync: async (key, values) => {
-            await writePrefAsync(key, values.join(","));
+        let data = {};
+        for (let website of pref.split(','))
+        {
+            data[website] = await readPrefAsync(`website-${website}`, "");
         }
-    };
-    notification = {
-        isFocusedAsync: async () => document.hasFocus()
-    };
-}
+        return data;
+    },
+    writeTokenAsync: async (token, website) => {
+        const pref = await readPrefAsync("websites", "");
+        let websites;
+        if (pref === "") websites = [];
+        else websites = pref.split(',');
+        websites.push(website);
 
-if (typeof versions === 'undefined') {
-    // If versions is undefined, it means we are on the browser version
-    initBrowser();
-}
+        await writePrefAsync("websites", websites.join(','));
+        await writePrefAsync(`website-${website}`, token);
+    },
+    readPrefAsync: readPrefAsync,
+    writePrefAsync: writePrefAsync,
+    readPrefArrayAsync: async (key) => {
+        const pref = await readPrefAsync(key, "");
+        if (pref === "") return [];
+        return pref.split(",");
+    },
+    writePrefArrayAsync: async (key, values) => {
+        await writePrefAsync(key, values.join(","));
+    }
+};
+notification = {
+    isFocusedAsync: async () => document.hasFocus()
+};
